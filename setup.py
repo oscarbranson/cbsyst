96904ca8abffebfa38be109786c79b9631af9ffc
Oscar Branson <oscarbranson@gmail.com>
2017-06-14 21:29:24 +1000

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info[0] < 3:
               '*********************************************\n'))
 
 setup(name='cbsyst',
-      version='0.3.1',
+      version='0.3.2',
       description='Tools for calculating ocean C and B chemistry.',
       url='https://github.com/oscarbranson/cbsyst',
       author='Oscar Branson',
