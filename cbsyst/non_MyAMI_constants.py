cffc03f091d5bef3f9e865da2d6b95f5169b1e40
Oscar Branson <oscarbranson@gmail.com>
2017-06-23 18:28:11 +1000

    Param updates & pH Scales
    Removed approximate pH scale correction listed in Dickson et al (2007) Handbook (added exact correction at cbsyst level).
    Corrected KSi prescorr params
    Changed KF Parameterisation to match CO2SYS.

--- cbsyst/non_MyAMI_constants.py
+++ cbsyst/non_MyAMI_constants.py
@@ -15,6 +15,11 @@ def calc_KPs(TempC, Sal, P=None):
     Chapter 5, Section 7.2.5 of Dickson, Sabine and Christian
     (2007, http://cdiac.ornl.gov/oceans/Handbook_2007.html)
 
+    **WITHOUT APPROX PH SCALE CONVERSION IN CONSTANT**
+    (See footnote 5 in 'Best Practices' Guide)
+    This produces constants on SWS pH Scale.
+    Must be converted to Total scale before use.
+
     Parameters
     ----------
     TempC : array-like
@@ -31,17 +36,17 @@ def calc_KPs(TempC, Sal, P=None):
     TempK = TempC + 273.15
     lnTempK = np.log(TempK)
     KP1 = np.exp(-4576.752 / TempK +
-                 115.525 +
+                 115.54 +
                  -18.453 * lnTempK +
                  (-106.736 / TempK + 0.69171) * Sal**0.5 +
                  (-0.65643 / TempK - 0.01844) * Sal)
     KP2 = np.exp(-8814.715 / TempK +
-                 172.0883 +
+                 172.1033 +
                  -27.927 * lnTempK +
                  (-160.340 / TempK + 1.3566) * Sal**0.5 +
                  (0.37335 / TempK - 0.05778) * Sal)
     KP3 = np.exp(-3070.75 / TempK +
-                 -18.141 +
+                 -18.126 +
                  (17.27039 / TempK + 2.81197) * Sal**0.5 +
                  (-44.99486 / TempK - 0.09984) * Sal)
 
@@ -49,7 +54,7 @@ def calc_KPs(TempC, Sal, P=None):
     # Checked against CO2SYS
     if (P is not None):
         KP1 *= prescorr(P, TempC,
-                        *[-14.51, 0.1211, -0.321e-3, -2.67, 0.0427])
+                        *[-14.51, 0.1211, -0.000321, -2.67, 0.0427])
         KP2 *= prescorr(P, TempC,
                         *[-23.12, 0.1758, -2.647e-3, -5.15, 0.09])
         KP3 *= prescorr(P, TempC,
@@ -66,6 +71,11 @@ def calc_KSi(TempC, Sal, P=None):
 
     Chapter 5, Section 7.2.6 of Dickson, Sabine and Christian
     (2007, http://cdiac.ornl.gov/oceans/Handbook_2007.html)
+
+    **WITHOUT APPROX PH SCALE CONVERSION IN CONSTANT**
+    (See footnote 5 in 'Best Practices' Guide)
+    This produces constants on SWS pH Scale.
+    Must be converted to Total scale before use.
 
     Parameters
     ----------
@@ -84,18 +94,17 @@ def calc_KSi(TempC, Sal, P=None):
     Istr = 19.924 * Sal / (1000 - 1.005 * Sal)
 
     KSi = np.exp(-8904.2 / TempK +
-                 117.385 +
+                 117.4 +
                  -19.334 * np.log(TempK) +
                  (-458.79 / TempK + 3.5913) * Istr**0.5 +
                  (188.74 / TempK - 1.5998) * Istr +
-                 (-12.1652 / TempK + 0.07871) * Istr**2 +
-                 np.log(1 - 0.001005 * Sal))
+                 (-12.1652 / TempK + 0.07871) * Istr**2) * (1 - 0.001005 * Sal)
 
     # parameters from Table 5 of Millero 2007 (doi:10.1021/cr0503557)
     # Checked against CO2SYS
     if (P is not None):
         KSi *= prescorr(P, TempC,
-                        *[-29.48, 0.1622, 2.608e-3, -2.84, 0])
+                        *[-29.48, 0.1622, -2.608e-3, -2.84, 0])
         # Note: Same as Boric acid constants
 
     return {'KSi': KSi}
@@ -105,8 +114,10 @@ def calc_KF(TempC, Sal, P=None):
     """
     Calculate equilibrium constants for HF.
 
-    Chapter 5, Section 7.2.4 of Dickson, Sabine and Christian
-    (2007, http://cdiac.ornl.gov/oceans/Handbook_2007.html)
+    Dickson, A. G. and Riley, J. P., Marine Chemistry 7:89-99, 1979
+
+    This produces constants on SWS pH Scale.
+    Must be converted to Total scale before use.
 
     Parameters
     ----------
@@ -122,9 +133,15 @@ def calc_KF(TempC, Sal, P=None):
     dict of KF
     """
     TempK = TempC + 273.15
-    KF = np.exp(874. / TempK +
-                -9.68 +
-                0.111 * Sal**0.5)
+    Istr = 19.924 * Sal / (1000 - 1.005 * Sal)
+    # Dickson, A. G. and Riley, J. P., Marine Chemistry 7:89-99, 1979
+    KF = np.exp(1590.2 / TempK - 12.641 + 1.525 * Istr**0.5) * (1 - 0.001005 * Sal)
+
+    # Chapter 5, Section 7.2.4 of Dickson, Sabine and Christian
+    # (2007, http://cdiac.ornl.gov/oceans/Handbook_2007.html)
+    # KF = np.exp(874. / TempK +
+    #             -9.68 +
+    #             0.111 * Sal**0.5)
 
     # parameters from Table 5 of Millero 2007 (doi:10.1021/cr0503557)
     # Checked against CO2SYS
