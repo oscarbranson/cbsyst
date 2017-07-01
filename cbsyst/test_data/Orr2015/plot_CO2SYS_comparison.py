33988a6676f1df33ba15244923a3171b84370cda
Oscar Branson <oscarbranson@gmail.com>
2017-07-01 11:09:19 +1000

    blank plot_CO2SYS_comparison.py

---
+++ cbsyst/test_data/Orr2015/plot_CO2SYS_comparison.py
@@ -0,0 +1,18 @@
+"""
+TODO: Script to compare CO2SYS.m output to cbsyst output.
+Comparison similar to Orr et al (2015)
+
+Parameters
+----------
+raw_data : str
+    Path to GLODAP-WOA comparison data.
+co2sys_data : str
+    Path to output from CO2SYS.m
+output_dir : str
+    Output directory for plots
+
+Returns
+-------
+Prints a range of statistics comparing CO2SYS and cbsyst.
+Creates a variety of plots with statistics in output_dir.
+"""
