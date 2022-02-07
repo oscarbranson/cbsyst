b0821ccfce0d6e6f59f0e1bec76bd1c63c938e64
Oscar Branson <ob266@cam.ac.uk>
2022-02-07 16:47:44 +0000

    renameed Functional_Forms

--- cbsyst/constants/K_functions.py
+++ cbsyst/constants/K_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def type_K1K2(p, TK, lnTK, S):
+def fn_K1K2(p, TK, lnTK, S):
     """Calculate K1 or K2 from given parameters
 
     Parameters
@@ -28,7 +28,7 @@ def type_K1K2(p, TK, lnTK, S):
         p[4] * S * S
     )
     
-def type_KW(p, TK, lnTK, S, sqrtS):
+def fn_KW(p, TK, lnTK, S, sqrtS):
     """Calculate KW from given parameters.
 
     Parameters
@@ -57,7 +57,7 @@ def type_KW(p, TK, lnTK, S, sqrtS):
         p[6] * S
     )
     
-def type_KB(p, TK, lnTK, S, sqrtS):
+def fn_KB(p, TK, lnTK, S, sqrtS):
     """Calculate KB from given parameters.
 
     Parameters
@@ -66,7 +66,7 @@ def type_KB(p, TK, lnTK, S, sqrtS):
         parameters for K calculation
     TK : array-like
         Temperature in Kelvin
-    lnTK : arra-ylike
+    lnTK : array-like
         natural log of temperature in kelvin
     S : arry-like
         Salinity
@@ -92,7 +92,7 @@ def type_KB(p, TK, lnTK, S, sqrtS):
         p[11] * sqrtS * TK
     )
     
-def type_K0(p, TK, S):
+def fn_K0(p, TK, S):
     """Calculate K0 from given parameters.
 
     Parameters
@@ -116,7 +116,7 @@ def type_K0(p, TK, S):
         S * (p[3] - p[4] * TK + p[5] * TK * TK)
     )
 
-def type_KHSO4(p, TK, lnTK, S):
+def fn_KHSO4(p, TK, lnTK, S):
     Istr = (
         19.924 * S / (1000 - 1.005 * S)
     )
@@ -135,7 +135,7 @@ def type_KHSO4(p, TK, lnTK, S):
         + np.log(1 - 0.001005 * S)
     )
     
-def type_Ksp(p, TK, S, sqrtS):
+def fn_Ksp(p, TK, S, sqrtS):
     """Calculate Ksp from given parameters
 
     Parameters
@@ -150,29 +150,94 @@ def type_Ksp(p, TK, S, sqrtS):
         square root of salinity
     """
 
-K_params = {
-    'K0': {
-        'type': 'K0',
-        'p': [-60.2409, 9345.17, 23.3585, 0.023517, -0.023656E-2, 0.0047036E-4]
-    },
-    'K1': {
-        'type': 'K1K2',
-        'p': [-3633.86, 61.2172, -9.67770, 0.011555, -0.0001152]
-    },
-    'K2': {
-        'type': 'K1K2',
-        'p': [-471.78, -25.9290, 3.16967, 0.01781, -0.0001122]
-    },
-    'KW': {
-        'type': 'KW',
-        'p': [148.9652, -13847.26, -23.6521, 118.67, -5.977, 1.0495, -0.01615]
-    },
-    'KB': {
-        'type': 'KB',
-        'p': [-8966.90, -2890.53, -77.942, 1.728, -0.0996, 148.0248, 137.1942, 1.62142, -24.4344, -25.085, -0.2474, 0.053105]
-    },
-    'KHSO4': {
-        'type': 'KHSO4',
-        'p': [ 141.328, -4276.1, -23.093, 324.57, -13856, -47.986, -771.54, 35474, 114.723, -2698, 1776 ]
-    }
-}
+    return np.power(
+        10,
+        (
+            p[0] + 
+            p[1] * TK +
+            p[2] / TK +
+            p[3] * np.log10(TK) +
+            (p[4] + p[5] * TK + p[6] / TK) * sqrtS +
+            p[7] * S +
+            p[8] * S * sqrtS
+        ),
+    )
+
+def fn_KP(p, TK, lnTK, S, sqrtS):
+    """Calculate KP(s) from given parameters
+
+    Parameters
+    ----------
+    p : array-like
+        parameters for K calculation
+    TK : array-like
+        Temperature in Kelvin
+    lnTK : array-like
+        natural log of temperature in kelvin
+    S : arry-like
+        Salinity
+    sqrtS : array-like
+        square root of salinity
+    """
+
+    return np.exp(
+        p[0] / TK
+        + p[1]
+        + p[2] * lnTK
+        + (p[3] / TK + p[4]) * sqrtS
+        + (p[5] / TK + p[6]) * S
+    )
+
+def fn_KSi(p, TK, lnTK, S, sqrtS):
+    """Calculate KSi from given parameters
+
+    Parameters
+    ----------
+    p : array-like
+        parameters for K calculation
+    TK : array-like
+        Temperature in Kelvin
+    lnTK : array-like
+        natural log of temperature in kelvin
+    S : arry-like
+        Salinity
+    sqrtS : array-like
+        square root of salinity
+    """
+
+    Istr = 19.924 * S / (1000 - 1.005 * S)
+
+    return np.exp(
+        p[0] / TK + 
+        p[1] +
+        p[2] * np.log(TK) +
+        (p[3] / TK + p[4]) * Istr ** 0.5 +
+        (p[5] / TK + p[6]) * Istr +
+        (p[7] / TK + p[8]) * Istr ** 2
+    ) * (1 - 0.001005 * S)
+
+def fn_KF(p, TK, lnTK, S, sqrtS):
+    """Calculate KSi from given parameters
+
+    Parameters
+    ----------
+    p : array-like
+        parameters for K calculation
+    TK : array-like
+        Temperature in Kelvin
+    lnTK : array-like
+        natural log of temperature in kelvin
+    S : arry-like
+        Salinity
+    sqrtS : array-like
+        square root of salinity
+    """
+
+    return np.exp(
+        p[0] / TK + 
+        p[1] + 
+        p[2] * sqrtS
+    )
+    # Istr = 19.924 * S / (1000 - 1.005 * S)
+
+    # return np.exp(p[0] / TK + p[1] + p[2] * Istr ** 0.5) * (1 - 0.001005 * S)
