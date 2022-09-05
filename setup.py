0ec0528711a91462e9a6c646281fb0653065f3f3
Filipe Fernandes <ocefpaf@gmail.com>
2022-09-05 09:13:06 -0300

    move find to cfg and add name metadata here

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(packages=find_packages())
+setup(
+    # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
+    # does not work unless we put the name this here too.
+    name="cbsyst",
+)
