32dbcd4b8b65c26c1e08c6e09b3fdad43a472a17
Oscar Branson <oscarbranson@gmail.com>
2025-07-31 19:54:20 +0100

    minor fixes and error handling

--- cbsyst/units.py
+++ cbsyst/units.py
@@ -1,7 +1,7 @@
 import numpy as np
-from .dataclasses import CarbonSystemParams
+from .dataclasses import CBsystData
 
-CONCENTRATION_PARAMS = ["DIC", "TA", "CO2", "HCO3", "CO3", "BT", "PT", "SiT"]
+CONCENTRATION_PARAMS = ["DIC", "TA", "CO2", "HCO3", "CO3", "BT", "BO3", "BO4", "PT", "SiT"]
 GAS_PARAMS = ['pCO2', 'fCO2']
 ALKALINITY_PARAMS = ["CAlk", "BAlk", "PAlk", "SiAlk", "OH", "HSO4", "HF", "Hfree"]
 
@@ -14,30 +14,40 @@ UNIT_MULTIPLIERS = {
     "fmol": 1.0e15,
 }
     
-def convert_to_molar(params: CarbonSystemParams) -> None:
+def convert_to_molar(params: CBsystData) -> None:
     """Convert input units to molar"""
     multiplier = UNIT_MULTIPLIERS.get(params.unit, params.unit)
     
     if multiplier != 1:
         for param in CONCENTRATION_PARAMS:
+            if param not in params.__dataclass_fields__:
+                continue
             value = getattr(params, param)
             if value is not None:
                 setattr(params, param, np.divide(value, multiplier))
     
     for param in GAS_PARAMS:
+        if param not in params.__dataclass_fields__:
+            continue        
         value = getattr(params, param)
         if value is not None:
             setattr(params, param, np.divide(value, 1e6))
 
-def convert_from_molar(params: CarbonSystemParams) -> None:
+def convert_from_molar(params: CBsystData) -> None:
     """Convert results back to input units"""
     multiplier = UNIT_MULTIPLIERS.get(params.unit, params.unit)
 
     if multiplier != 1:
         for param in CONCENTRATION_PARAMS + ALKALINITY_PARAMS:
-            if param in params:
+            if param not in params.__dataclass_fields__:
+                continue
+            value = getattr(params, param)
+            if value is not None:
                 params[param] *= multiplier
     
     for param in GAS_PARAMS:
-        if param in params:
-            params[param] = np.multiply(params[param], 1e6)
+        if param not in params.__dataclass_fields__:
+            continue
+        value = getattr(params, param)
+        if value is not None:
+            params[param] = np.multiply(value, 1e6)
